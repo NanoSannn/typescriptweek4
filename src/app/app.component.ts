70ff6c24014391797617f32c25124dac8e9a03b9
vampire_pp_ <vampirepp11@gmail.com>
2021-11-23 16:53:25 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1 : number = 5;
+  n2 : number = 2;
+  picName :string="https://i.pinimg.com/564x/b9/0c/a9/b90ca9a9a18962e5a0741c77a3bb3f45.jpg";
+  
+  hi(){
+    alert("Fuck youd")
+  }
 }
